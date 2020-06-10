18f3a9a8e7030327eec32c63eb543a30110cc6d5
Hritika Rajput <hritika.rajput@dream11.com>
2020-06-10 19:03:57 +0530

    Updating problem3.sql,adding results

--- problem3.sql
+++ problem3.sql
@@ -1,8 +1,10 @@
-#Problem 3
-#Create a transaction which does the following things:
-#Create a new customer
-#New customer places an order for a motorcyle
-#And finally does the payment.
+/*
+Problem 3
+Create a transaction which does the following things:
+Create a new customer
+New customer places an order for a motorcyle
+And finally does the payment.
+*/
 
 start transaction;
 
@@ -11,6 +13,17 @@ select
     @customerNumberNew:=max(customerNumber)+1
 from
     customers;
+/*
++-------------------------------------------+
+| @customerNumberNew:=max(customerNumber)+1 |
++-------------------------------------------+
+|                                       506 |
++-------------------------------------------+
+1 row in set (0.00 sec)
+
+Query OK, 1 row affected (0.00 sec)
+*/
+
 
 /*Inserting a new customer in Customer table*/
 insert into customers(customerNumber,customerName, contactLastName, contactFirstName, phone, addressLine1, city, country)
@@ -22,12 +35,33 @@ select
     @orderNumberNew:=max(orderNumber)+1
 from
     orders;
+/*
++-------------------------------------+
+| @orderNumberNew:=max(orderNumber)+1 |
++-------------------------------------+
+|                               10435 |
++-------------------------------------+
+1 row in set (0.00 sec)
+
+Query OK, 1 row affected (0.00 sec)
+*/
+
 
 /* Selecting the maximum price bike*/
 select
     @motorCycleCode:= productCode,
     @price:=max(buyPrice)
 from products where productLine="Motorcycles" limit 1;
+/*
++-------------------------------+-----------------------+
+| @motorCycleCode:= productCode | @price:=max(buyPrice) |
++-------------------------------+-----------------------+
+| S10_1678                      |                 91.02 |
++-------------------------------+-----------------------+
+1 row in set (0.00 sec)
+
+Query OK, 1 row affected (0.00 sec)
+*/
 
 /*Inserting a new entry in order details*/
 insert into orders(orderNumber,orderDate,requiredDate,status,customerNumber)
@@ -41,6 +75,17 @@ values(@orderNumberNew,@motorCycleCode,5,@price,1);
 select 
     @quantity:=quantityOrdered
 from orderdetails where orderNumber=@orderNumberNew;
+/*
++----------------------------+
+| @quantity:=quantityOrdered |
++----------------------------+
+|                          5 |
++----------------------------+
+1 row in set (0.00 sec)
+
+Query OK, 1 row affected (0.00 sec)
+*/
+
 
 /*Inserting entry in payments table*/
 insert into payments(customerNumber,checkNumber,paymentDate,amount)
